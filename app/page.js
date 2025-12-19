63c07b76fd5ecc97591d5b85e7af4e42042f4b75
pratham <pratham@pratham-k-s.lan>
2025-12-19 14:42:33 +0530

    Initial commit of arecoid launching soon

--- app/page.js
+++ app/page.js
@@ -1,65 +1,60 @@
 import Image from "next/image";
+import The_Seasons from "next/font/local";
+import { Yeseva_One } from "next/font/google";
+
+const yesevaOne = Yeseva_One({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const theSeasons = The_Seasons({
+  src: "/The_Seasons.otf",
+});
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <main>
+      <nav className="w-full h-14 md:h-16 lg:h-20 bg-[#fff3e4] flex justify-center items-center">
+        <span
+          className={`${yesevaOne.className} text-2xl md:tex-3xl lg:text-4xl text-[#694a38]`}
+        >
+          Arecoid
+        </span>
+      </nav>
+      <div className="h-screen">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src={"/website_bg_arecoid_16_9.png"}
+          alt="Image of areca leaf plates kept on a table"
+          quality={100}
+          fill
+          sizes="100vw"
+          className="object-cover -z-10 relative"
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div className="relative w-full h-full opacity-50 bg-black"></div>
+        <div className="absolute top-0 w-full flex flex-col gap-10 lg:gap-16 justify-center items-center h-full">
+          <div
+            className={`${theSeasons.className} text-center text-6xl md:text-7xl lg:text-8xl text-[#fff3e4]`}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <h1>Launching</h1>
+            <h1>Soon</h1>
+          </div>
+          <div className="text-[#fff3e4] w-full lg:w-1/3 text-center text-xs md:text-lg">
+            <p className="mx-8">
+              Redefining global dinnerware with the strength of nature and the
+              spirit of sustainability. A new chapter by Parashakthi Industries
+            </p>
+          </div>
+          <div className="text-center flex justify-center">
+            <input
+              placeholder="jhon@example.com"
+              className="text-xs md:text-lg md:px-8 bg-[#fff3e4] outline-none px-4 py-2 text-[#694a38]"
+            ></input>
+            <button className="text-xs md:text-lg px-2 md:px-8 bg-[#694a38] text-[#fff3e4]">
+              Notify Me
+            </button>
+          </div>
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
