b01c3759f524969eb8a89d9ed3b4d0f100607a5c
Pratham K S <prathamks2302@gmail.com>
2025-12-31 01:13:42 +0530

    added copyright text to footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -9,63 +9,70 @@ const atkinsonHyperlegible = Atkinson_Hyperlegible({
 
 export default function Footer() {
   return (
-    <footer className="px-6 md:px-8 xl:px-16 py-10 xl:py-16 bg-arecoidCream flex flex-col xl:flex-row xl:justify-between gap-10">
-      <div className="flex flex-col">
-        <span className="flex py-3">
-          <Image
-            src="/nav/arecoid_logo.png"
-            alt="Arecoid Logo"
-            width={105.12}
-            height={100}
-            priority
-          />
-        </span>
-        <p
-          className={`${atkinsonHyperlegible.className} text-sm w-3/4 xl:w-1/2 text-arecoidBrown`}
-        >
-          Manufacturer of eco-friendly areca leaf dinnerware from India,
-          supplying bulk and export-ready products.
-        </p>
-      </div>
-      <div className="flex flex-col xl:flex-row gap-8 xl:gap-16">
-        <div className="flex flex-col gap-2">
-          <h2
-            className={`${atkinsonHyperlegible.className} text-[18px] font-bold leading-6 text-arecoidBrown`}
+    <footer className="px-6 md:px-8 xl:px-16 py-10 xl:py-16 bg-arecoidCream">
+      <div className="flex flex-col xl:flex-row xl:justify-between gap-10">
+        <div className="flex flex-col">
+          <span className="flex py-3">
+            <Image
+              src="/nav/arecoid_logo.png"
+              alt="Arecoid Logo"
+              width={105.12}
+              height={100}
+              priority
+            />
+          </span>
+          <p
+            className={`${atkinsonHyperlegible.className} text-sm w-3/4 xl:w-1/2 text-arecoidBrown`}
           >
-            Links
-          </h2>
-          <ul
-            className={`${atkinsonHyperlegible.className} text-[14px] text-arecoidBrown space-y-2`}
-          >
-            <li>
-              <Link href="/about">About Us</Link>
-            </li>
-            <li>
-              <Link href="/products">Products</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contact</Link>
-            </li>
-            <li>
-              <Link href="/manufacturing">Manufacturing</Link>
-            </li>
-          </ul>
+            Manufacturer of eco-friendly areca leaf dinnerware from India,
+            supplying bulk and export-ready products.
+          </p>
         </div>
-        <div className="flex flex-col gap-2">
-          <h2
-            className={`${atkinsonHyperlegible.className} text-[18px] font-bold leading-6 text-arecoidBrown`}
-          >
-            Contact Us
-          </h2>
-          <ul
-            className={`${atkinsonHyperlegible.className} text-[14px] text-arecoidBrown space-y-2`}
-          >
-            <li>📧 sales@arecoid.in</li>
-            <li>📞 +91-8618168526</li>
-            <li>📍 India</li>
-          </ul>
+        <div className="flex flex-col xl:flex-row gap-8 xl:gap-16">
+          <div className="flex flex-col gap-2">
+            <h2
+              className={`${atkinsonHyperlegible.className} text-[18px] font-bold leading-6 text-arecoidBrown`}
+            >
+              Links
+            </h2>
+            <ul
+              className={`${atkinsonHyperlegible.className} text-[14px] text-arecoidBrown space-y-2`}
+            >
+              <li>
+                <Link href="/about">About Us</Link>
+              </li>
+              <li>
+                <Link href="/products">Products</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link href="/manufacturing">Manufacturing</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="flex flex-col gap-2">
+            <h2
+              className={`${atkinsonHyperlegible.className} text-[18px] font-bold leading-6 text-arecoidBrown`}
+            >
+              Contact Us
+            </h2>
+            <ul
+              className={`${atkinsonHyperlegible.className} text-[14px] text-arecoidBrown space-y-2`}
+            >
+              <li>📧 sales@arecoid.in</li>
+              <li>📞 +91-8618168526</li>
+              <li>📍 India</li>
+            </ul>
+          </div>
         </div>
       </div>
+      <p
+        className={`${atkinsonHyperlegible.className} text-xs text-center text-arecoidBrown pt-16`}
+      >
+        © 2026 Arecoid – Parashakthi Industries. All rights reserved.
+      </p>
     </footer>
   );
 }
